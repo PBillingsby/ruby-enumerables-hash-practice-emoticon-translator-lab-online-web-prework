62990ee0ad784a41baeaba05d87051634dca1498
Phil Billingsby <paulbillingsby@gmail.com>
2019-09-21 18:28:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,11 @@
 # require modules here
+require 'yaml'
 
 def load_library
   # code goes here
+  library = YAML.load_file('emoticons.yml')
+  library
 end
-
 def get_japanese_emoticon
   # code goes here
 end
